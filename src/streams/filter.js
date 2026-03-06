fc4f179c109d7296d99812c9a36e261712785e3d
Mariia Kulikova <mariya.kulikova818@gmail.com>
2026-03-06 12:18:31 +0100

    feat: implement filter functionality using Transform Stream

--- src/streams/filter.js
+++ src/streams/filter.js
@@ -1,9 +1,40 @@
+import { Transform } from "node:stream";
+
 const filter = () => {
-  // Write your code here
-  // Read from process.stdin
-  // Filter lines by --pattern CLI argument
-  // Use Transform Stream
-  // Write to process.stdout
+	const args = process.argv;
+	const i = args.indexOf("--pattern");
+	const pattern = i !== -1 ? args[i + 1] : undefined;
+
+	if (!pattern) {
+		process.stderr.write("No arguments\n");
+		process.exitCode = 1;
+		return;
+	}
+
+	let tail = "";
+	const patternTransform = new Transform({
+		transform(chunk, enc, cb) {
+			const strChunk = String(chunk);
+			tail += strChunk;
+			const lines = tail.split("\n");
+			tail = lines.pop();
+			lines.forEach((el, idx, arr) => {
+				if (el.includes(pattern)) {
+					this.push(el + "\n");
+				}
+			});
+			cb();
+		},
+
+		flush(cb) {
+			if (tail.length > 0 && tail.includes(pattern)) {
+				this.push(tail + "\n");
+			}
+			cb();
+		},
+	});
+
+	process.stdin.pipe(patternTransform).pipe(process.stdout);
 };
 
 filter();
