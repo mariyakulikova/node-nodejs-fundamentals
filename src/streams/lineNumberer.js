13394e53f83bed46f7e3b7af8cd4ae4293c03557
Mariia Kulikova <mariya.kulikova818@gmail.com>
2026-03-06 12:19:24 +0100

    feat: implement line numbering functionality using Transform Stream

--- src/streams/lineNumberer.js
+++ src/streams/lineNumberer.js
@@ -1,8 +1,27 @@
+import { Transform } from "node:stream";
+
 const lineNumberer = () => {
-  // Write your code here
-  // Read from process.stdin
-  // Use Transform Stream to prepend line numbers
-  // Write to process.stdout
+	let tail = "";
+	let counter = 1;
+	const patternTransform = new Transform({
+		transform(chunk, enc, cb) {
+			const strChunk = String(chunk);
+			tail += strChunk;
+			const lines = tail.split("\n");
+			tail = lines.pop();
+			lines.forEach((el, idx, arr) => this.push(counter++ + " " + el + "\n"));
+			cb();
+		},
+
+		flush(cb) {
+			if (tail.length > 0) {
+				this.push(counter + " " + tail + "\n");
+			}
+			cb();
+		},
+	});
+
+	process.stdin.pipe(patternTransform).pipe(process.stdout);
 };
 
 lineNumberer();
