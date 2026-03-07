3d5d09ff46fbbd223f5594298dbabf90f153c4e5
Mariia Kulikova <mariya.kulikova818@gmail.com>
2026-03-07 12:23:25 +0100

    feat: implement findByExt function to recursively find files by extension from CLI arguments

--- src/fs/findByExt.js
+++ src/fs/findByExt.js
@@ -1,7 +1,33 @@
+import { readdir } from "node:fs/promises";
+import path from "node:path";
+
 const findByExt = async () => {
-  // Write your code here
-  // Recursively find all files with specific extension
-  // Parse --ext CLI argument (default: .txt)
+	// Write your code here
+	// Recursively find all files with specific extension
+	// Parse --ext CLI argument (default: .txt)
+	const args = process.argv.slice(2);
+	const i = args.indexOf("--ext");
+	const ext = i !== -1 ? args[i + 1] : ".txt";
+
+	const currDir = process.cwd();
+
+	const findFiles = async (dir) => {
+		const entries = await readdir(dir, { withFileTypes: true });
+
+		for (const entry of entries) {
+			const fullPath = path.join(dir, entry.name);
+
+			if (entry.isDirectory()) {
+				await findFiles(fullPath);
+			}
+
+			if (entry.isFile() && entry.name.endsWith(ext)) {
+				console.log(fullPath);
+			}
+		}
+	};
+
+	await findFiles(currDir);
 };
 
 await findByExt();
